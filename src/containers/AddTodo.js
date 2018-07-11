0cce509aa29c1f5654b69e3e9ca07854e1a7cb66
baoxun <baoweipeng@51zhiyin.com>
2018-07-11 19:10:35 +0800

    测试

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const AddTodo = () => (
-    <div>
-        <form>
-            <input />
-            <button>添加按钮</button>
-        </form>
-    </div>
-)
+const AddTodo = () => {
+    let input
+
+    return (
+        <div>
+            <form onSubmit={e => {
+                e.preventDefault()
+                if (!input.value.trim()) {
+                    return
+                }
+            }}>
+                <input />
+                <button type="submit">添加按钮</button>
+            </form>
+        </div>
+    )
+}
 
 export default AddTodo
